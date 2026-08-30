c671eaff329d27a78a412ee4358ffc6ab4c79040
jonathanQ77 <jonathan.quesnel91@gmail.com>
2026-08-31 00:52:57 +0200

    Mise en place : -> arboressence MVC -> controller (render) PageController (page home , about) -> Router ->gestions des routes autorisés -> htaccess ->install composer + parametrage dans index.php

---
+++ public/index.php
@@ -0,0 +1,15 @@
+<?php
+
+require __DIR__ . '/../vendor/autoload.php';
+// définition de la constante pour le chemin vers le dossier public
+define('APP_ROOT', dirname(__DIR__));
+
+use App\Routing\Router;
+
+$router = new Router();
+$router->handleRequest($_SERVER['REQUEST_URI']);
+/*
+ *
+$pageController = new PageController();
+$pageController->home();
+*/
